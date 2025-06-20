8687626c9dc591860d42158089e4735ca94c2732
SoultaneRaqi <soultaneraqi1@gmail.com>
2025-06-20 01:12:42 +0100

    modification in add delete and edit

--- add_student.php
+++ add_student.php
@@ -3,10 +3,7 @@ require_once "connection.php";
 session_start();
 
 
-if (!isset($_SESSION['user'])) {
-    header("Location: login.php");
-    exit();
-}
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = trim($_POST['name'] ?? '');
--- edit_student.php
+++ edit_student.php
@@ -4,10 +4,7 @@ require_once"connection.php";
 
 session_start();
 
-if (!isset($_SESSION['user'])) {
-    header("Location: login.php");
-    exit();
-}
+
 
 
 if (!isset($_GET["id"])){
